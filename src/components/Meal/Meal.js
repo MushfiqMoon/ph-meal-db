2f090df100ec3fcde01ebfc9142f7487c323e187
MushfiqMoon <mushfiq.moon013@google.com>
2022-09-28 11:29:06 +0600

    lazy load added

--- src/components/Meal/Meal.js
+++ src/components/Meal/Meal.js
@@ -1,25 +1,27 @@
 import React from 'react'
+
 import { Card, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
 import { faSquarePlus } from '@fortawesome/free-solid-svg-icons'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 
 export const Meal = ({ meal, addHandel }) => {
 
     // console.log(meal)
 
-    const {strMealThumb,strMeal} = meal
+    const { strMealThumb, strMeal } = meal
     return (
         <Card className="mb-4">
-            <Card.Img variant="top" src={strMealThumb} />
+            <LazyLoadImage
+                src={strMealThumb} />
             <Card.Body>
                 <Card.Title>{strMeal}</Card.Title>
                 <Card.Text>
                     Some quick example text to build on the card title and make up the
                     bulk of the card's content.
                 </Card.Text>
-                <Button onClick={()=>addHandel(meal)} variant="dark text-white"><FontAwesomeIcon icon={faSquarePlus} /> Add Item</Button>
+                <Button onClick={() => addHandel(meal)} variant="dark text-white"><FontAwesomeIcon icon={faSquarePlus} /> Add Item</Button>
             </Card.Body>
         </Card>
     )
